744d0eb8c44f6b7dcfcd7d2a56db67420c8f0c4e
Mara Lupescu <mara@lupescu.ca>
2025-11-12 16:52:44 -0500

    Add frontend files

---
+++ app/static/index.js
@@ -0,0 +1,49 @@
+// Get data from a hidden element
+const roomData = document.getElementById("room-data");
+const roomId = roomData.getAttribute("data-room-id");
+const username = roomData.getAttribute("data-username");
+const userId = roomData.getAttribute("data-user-id");
+
+// set up WebSocket connection
+const ws = new WebSocket(`ws://localhost:8000/ws/chat/${roomId}/${userId}?username=${username}`);
+
+// event trackers 
+ws.onopen = () => {
+    console.log("Connection established");
+};
+
+ws.onclose = () => {
+    console.log("Connection closed");
+};
+
+// receive messages
+ws.onmessage = (event) => {
+    const messages = document.getElementById("messages");
+    const messageData = JSON.parse(event.data);
+    const message = document.createElement("div");
+
+    // Define styles depending on the sender
+    if (messageData.is_self) {
+        message.className = "p-2 my-1 bg-blue-500 text-white rounded-md self-end max-w-xs ml-auto";
+    } else {
+        message.className = "p-2 my-1 bg-gray-200 text-black rounded-md self-start max-w-xs";
+    }
+
+    message.textContent = messageData.text;
+    messages.appendChild(message);
+    messages.scrollTop = messages.scrollHeight; // Auto scroll down
+};
+
+// send message
+function sendMessage() {
+    const input = document.getElementById("messageInput");
+    if (input.value.trim()) {
+        ws.send(input.value);
+        input.value = '';
+    }
+}
+document.getElementById("messageInput").addEventListener("keypress", (e) => {
+    if (e.key === "Enter") { // sends message by enter
+        sendMessage();
+    }
+});
